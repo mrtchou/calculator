b2628f26c0d09f7efdaa00dc0425710be20c799c
mrtchou <77937111+mrtchou@users.noreply.github.com>
2021-12-11 20:07:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const buttons = document.querySelectorAll(".btn");
 
-console.log(buttons);
+const buttons = document.querySelectorAll(".btn");
+const resultBarreH3 = document.getElementById("result");
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        resultBarreH3.textContent += event.target.id;
+    });
+});
+
+
+equal.addEventListener('click', () => {
+    //eval est une function intern a js qui calcule mathematiquement ce qui entre les parentheses
+    resultBarreH3.textContent = eval(resultBarreH3.textContent);
+});
+
+clear.addEventListener('click', () => {
+    resultBarreH3.textContent = " ";
+})
